8de8c2e38e0aa952744e56bdc658aefb24731f81
Topaanbgs <56524651+Topaanbgs@users.noreply.github.com>
2026-01-29 20:26:16 +0800

    Refactor Firebase config to use environment variables
    Updated Firebase configuration to use environment variables for sensitive data.

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBxjmYnkkI1JRWHJWV_F8g8fi0QixZBxMM",
-  authDomain: "loanhub-e961b.firebaseapp.com",
-  projectId: "loanhub-e961b",
-  storageBucket: "loanhub-e961b.appspot.com",
-  messagingSenderId: "545058752660",
-  appId: "1:545058752660:web:1abafd02b8ddd9e3d24c81",
-  measurementId: "G-52MYGFH59Q"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
